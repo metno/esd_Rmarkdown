6cc23c494c5424e283caeaca5a9b233fcde96f83
Rasmus Benestad <rasmusb@vis-m1.met.no>
2016-11-10 08:18:23 +0000

    CixPAG spells and means

--- CixPAG/meanspell.R
+++ CixPAG/meanspell.R
@@ -3,27 +3,28 @@
 
 it <- 'djf'
 cold <- TRUE
-if (cold) is <- 2 else is <- 1
 threshold <- 0
+if (cold) is <- 2 else is <- 1
 
 library(esd)
 ss <- select.station(src='ecad',param='tg',nmin=75)
 d <- dim(ss)
 x <- rep(NA,d[1]); y <- x
-for (is in 1:d[1]) {
-  Tx <- station(ss[is,])
+for (i in 1:d[1]) {
+  Tx <- station(ss[i,])
   print(loc(Tx))
-  z <- subset(Tx,is=is)
+  z <- subset(Tx,is=i)
   ## Make sure that there are values above and below the given threshold - otherwise
   ## spell will not work.
-  if ( (sum(z > threshold,na.rm=TRUE)>100) & (sum(z < threshold,na.rm=TRUE)> 100) ) {
+  if ( (sum(z > threshold,na.rm=TRUE)>1000) & (sum(z < threshold,na.rm=TRUE)> 1000) ) {
     s <- spell(z,threshold=threshold)
-    y[is] <- mean(subset(subset(s,is=2),it=it),na.rm=TRUE)
-    x[is] <- mean(subset(z,it=it),na.rm=TRUE)
-    plot(x,y,main='Mean temperature & mean length of freezing spells',
+    y[i] <- mean(subset(subset(s,is=is),it=it),na.rm=TRUE)
+    x[i] <- mean(subset(z,it=it),na.rm=TRUE)
+    plot(x,y,main=paste('Mean',it,'temperature & mean length of intervals',
+                        c('below','above')[c(cold,!cold)],threshold,'C'),
         sub='source: ECA&D',pch=19,col=rgb(0.5,0,0,0.3),
-        xlab=expression(bar(T)),ylab=expression(bar(tau[T < T0])))
-    calfit <- data.frame(x=max(x,na.rm=TRUE) - x[is.finite(x) & is.finite(y)],
+        xlab=expression(bar(T)),ylab=expression(bar(tau)))
+    calfit <- data.frame(x=x[is.finite(x) & is.finite(y)],
                          y=y[is.finite(x) & is.finite(y)])
     attr(calfit,'max(x)') <- max(x,na.rm=TRUE)
     #fit <- lm(y ~ I(x) + I(x^2),data=calfit)
@@ -37,9 +38,9 @@ for (is in 1:d[1]) {
 attr(x,'description') <- paste(it,'mean temperature (degC)')
 if (cold) attr(y,'description') <- 'mean cold spell length (days)' else
           attr(y,'description') <- 'mean warm spell length (days)'
-attr(y,'threshold') <- treshold
+attr(y,'threshold') <- threshold
 attr(x,'label') <- expression(bar(T))
-attr(y,'label') <- xpression(bar(tau[T < T0]))
+attr(y,'label') <- expression(bar(tau[T < T0]))
 meanspell <- data.frame(meanT=x,meanL=y)
 attr(meanspell,'fit') <- fit
 save(meanspell,file=paste('meanspell',it,c('below','above')[c(cold,!cold)],threshold,'.rda',sep=''))
