6a4919c65f844b9ef4c6a9024d4ad3f8d0773c88
Rasmus Benestad <rasmusb@pc4409.pc.met.no>
2017-09-15 14:49:54 +0200

    mimoza + Bangladesh

--- EU-Circle/bangladesh-imilast.R
+++ EU-Circle/bangladesh-imilast.R
@@ -4,7 +4,7 @@
 
 library(esd)
 # 03 000001 001 1989010100 1989    1    1   0   -170   61.5    961.24
-imilast.storms <- '~/disk1/IMILAST/imilast.cyclones/ERAinterim_1.5_NH_M03_19890101_20090331_ST.txt'
+imilast.storms <- '~/Disk1/IMILAST/imilast.cyclones/ERAinterim_1.5_NH_M03_19890101_20090331_ST.txt'
 Z <- events2trajectory(read.imilast(imilast.storms))
 ##Z <- subset(Z,is=list(lon=c(80,120),lat=c(10,30)))
 map(Z)
